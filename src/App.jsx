1b81e8252ebe3dedc8a13379d95f1441d0a6f9e8
PranavShende-codes <pranavshede40@gmail.com>
2025-02-15 15:04:53 +0530

    Initial commit - React To-Do List setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<h1 className='text-amber-500 font-bold underline'>hi</h1>
+    </>
+  )
+}
+
+export default App
