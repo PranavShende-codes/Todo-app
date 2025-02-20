f22edb9f7ffac6eaa466e8444560f71287ebc3e5
PranavShende-codes <pranavshede40@gmail.com>
2025-02-20 17:28:47 +0530

    Added input field and button for adding tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
+  const [Task, setTask] = useState("");
   return (
     <>
-<h1 className='text-amber-500 font-bold underline'>hi</h1>
+      <div className="flex min-h-screen justify-center bg-gray-100  ">
+        <div className="bg-white p-6 rounded-lg shadow-lg w-96 ">
+          <h1 className="text-2xl font-bold text-center mb-4">To-do list</h1>
+          <input
+            className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring focus:border-blue-300 "
+            type="text"
+            placeholder="Type your task"
+            value={Task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg mt-4 transition-all w-full
+          ">Add Task</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
