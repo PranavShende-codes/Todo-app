7d3b01be599c96c6145240ef794bab4d3fc3ece1
PranavShende-codes <pranavshede40@gmail.com>
2025-02-21 15:24:09 +0530

    Added task list functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import "./App.css";
 
 function App() {
   const [Task, setTask] = useState("");
+  const [Tasks,setTasks]=useState([]);
+
+  function addTask() {
+    if (Task.trim() === "") return;
+    setTasks([...Tasks,Task]);
+    setTask("");
+  }
   return (
     <>
       <div className="flex min-h-screen justify-center bg-gray-100  ">
@@ -15,8 +22,15 @@ function App() {
             value={Task}
             onChange={(e) => setTask(e.target.value)}
           />
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg mt-4 transition-all w-full
-          ">Add Task</button>
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg mt-4 transition-all w-full cursor-pointer" onClick={addTask}>Add Task</button>
+
+          <ul className="mt-4">
+            {Tasks.map((t, index) => (
+              <li key={index}
+              className="bg-gray-200 p-2 rounded-lg mt-2 overflow-auto text-gray-800"
+              >{t}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
