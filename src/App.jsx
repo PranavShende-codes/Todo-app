ed604af32a8c5e3a2f7cee06d5fad11ce5732a30
PranavShende-codes <pranavshede40@gmail.com>
2025-02-24 16:16:02 +0530

    Added delete task functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import "./App.css";
 
 function App() {
   const [Task, setTask] = useState("");
-  const [Tasks,setTasks]=useState([]);
+  const [Tasks, setTasks] = useState([]);
 
   function addTask() {
     if (Task.trim() === "") return;
-    setTasks([...Tasks,Task]);
+    setTasks([...Tasks, Task]);
     setTask("");
   }
+  const removeTask = (index) => {
+    setTasks(Tasks.filter((t, i) => i !== index));
+  };
   return (
     <>
       <div className="flex min-h-screen justify-center bg-gray-100  ">
@@ -22,13 +25,27 @@ function App() {
             value={Task}
             onChange={(e) => setTask(e.target.value)}
           />
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg mt-4 transition-all w-full cursor-pointer" onClick={addTask}>Add Task</button>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg mt-4 transition-all w-full cursor-pointer"
+            onClick={addTask}
+          >
+            Add Task
+          </button>
 
-          <ul className="mt-4">
+          <ul className="mt-4 ">
             {Tasks.map((t, index) => (
-              <li key={index}
-              className="bg-gray-200 p-2 rounded-lg mt-2 overflow-auto text-gray-800"
-              >{t}</li>
+              <li
+                key={index}
+                className="bg-gray-200 p-2 rounded-lg mt-2 flex overflow-auto text-gray-800"
+              >
+                <div className="flex-1 ">{t}</div>
+                <button
+                  className="bg-red-500 hover:bg-red-600 text-white  py-1 px-2 rounded-lg ml-5 transition-all cursor-pointer "
+                  onClick={() => removeTask(index)}
+                >
+                  Delete
+                </button>
+              </li>
             ))}
           </ul>
         </div>
